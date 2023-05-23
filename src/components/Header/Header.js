cf8893ce9c2c2b173e3f34c6154806be268ddc3b
Hakan Urtimur <dythakanurtimur@gmail.com>
2023-05-23 18:40:06 +0300

    Adding NavLink

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,39 @@
-
 import { NavLink } from "react-router-dom";
 import Logo from "../../img/logo/logo";
-import classes from './Header.module.css'
-
+import classes from "./Header.module.css";
 
 function Header() {
-
-
-    return (
-        <header className={classes.header}>
-            <Logo />
-            <nav>
-            <ul className={classes.list}>
-                <li><NavLink to='/' style={{}}>Home Page</NavLink></li>
-                <li><NavLink to= '/drumming'>Drumming Page</NavLink></li>
-            </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header className={classes.header}>
+      <Logo />
+      <nav>
+        <ul className={classes.list}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home Page
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/drumming"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Drumming Page
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
